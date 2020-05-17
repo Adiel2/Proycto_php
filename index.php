81f99fe7a6a6163e8617d73da27272c60ed25280
Adieltrejo2 <cmelgar415@gmail.com>
2020-05-17 13:20:27 -0600

    Primera version con Git

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>Prueba de Ejercicios</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sed dicta alias, iure labore eum commodi, officiis nesciunt fugiat soluta architecto est sapiente molestias, suscipit cupiditate praesentium dignissimos corrupti vitae! </p>
+    
+</body>
+</html>
