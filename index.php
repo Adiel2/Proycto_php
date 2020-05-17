72175b752be0e08882c127a1a2600eb6993ebadb
Adieltrejo2 <cmelgar415@gmail.com>
2020-05-17 14:07:32 -0600

    Prueba numeros 2

--- index.php
+++ index.php
@@ -10,5 +10,7 @@
 <h1>Prueba de Ejercicios</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sed dicta alias, iure labore eum commodi, officiis nesciunt fugiat soluta architecto est sapiente molestias, suscipit cupiditate praesentium dignissimos corrupti vitae! </p>
     
+    <h2>Prueba numero  2</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ex enim laudantium ad dolor expedita pariatur aliquid voluptatem facilis laborum, totam repellendus deserunt numquam perspiciatis eveniet nihil sunt blanditiis cumque?</p>
 </body>
 </html>
